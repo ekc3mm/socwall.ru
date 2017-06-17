519095f83cbe1cacc47c5ba9534e622ba1e3b5ed
ekc3mm <ekc3mm@gmail.com>
2017-06-17 23:09:04 +0300

    ok

---
+++ app.php
@@ -0,0 +1,10 @@
+<?php 
+	require_once 'core/model.php';
+	require_once 'core/view.php';
+	require_once 'core/controller.php';
+	require_once 'core/route.php';
+	require_once 'include/rb.php';
+
+	R::setup( 'mysql:host=localhost;dbname=socwall', 'root', '' );
+	Route::start();
+ ?>
