697d992941f3b68d31bfa81ea0a5babb967184f8
ekc3mm <ekc3mm@gmail.com>
2017-06-17 23:42:43 +0300

    Update app.php

--- app/app.php
+++ app/app.php
@@ -3,7 +3,7 @@
 	require_once 'core/view.php';
 	require_once 'core/controller.php';
 	require_once 'core/route.php';
-	require_once '/include/rb.php';
+	require_once 'include/rb.php';
 
 	R::setup( 'mysql:host=localhost;dbname=socwall', 'root', '' );
 	Route::start();
